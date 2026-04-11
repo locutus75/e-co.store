b657a00936f095897cd91ac17784fb9ba91a1c07
locutus <locutus75@outlook.com>
2026-04-11 16:57:43 +0200

    feat: add allowed origins to server actions configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: '150mb',
+      allowedOrigins: ['e-co.store', 'e-co.store:4000', 'www.e-co.store', 'localhost:4000', 'localhost:3000'],
     },
   },
 };
